6091f33a26fffc8e1fef649f39af2e3c1aff0f7a
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 00:58:23 +1300

    Readding knocker

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 chmod 600 ./deploy_key
 
-ssh-agent echo "ssh-add ./deploy_key ; scp -v -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
+dub fetch knocker
+
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
