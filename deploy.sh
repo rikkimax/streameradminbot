c152ef89edd9689401f168ed315617a5d5a2fe12
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 00:14:39 +1300

    Okay definitely last try

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@ restorecon -R -v ~/.ssh
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
 chmod 600 ~/.ssh/sshDeployKey
 
-#echo "Setting ssh-agent"
 
-#ssh-agent bash
 
 echo "Adding cert"
 
 ssh-add ~/.ssh/sshDeployKey
 
 echo "Pre SCP copying"
+echo "Setting ssh-agent"
 
-scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/
+ssh-agent echo "scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
 
 echo "Post SCP copying"
