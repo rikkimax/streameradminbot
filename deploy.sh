7fd53193a0e1d0497e06f54c0326336137bd948f
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 22:55:18 +1300

    So ugh, can we even ssh at all?

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 dub fetch knocker
 dub run knocker --build=release -- vps1.cattermole.co.nz 
 echo "Deploying to server"
-scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
+ssh alphabot@vps1.cattermole.co.nz date
+# scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
