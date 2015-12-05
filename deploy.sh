3c97670bc7f20ee25427613c0ff018ce25f27f0d
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 23:09:37 +1300

    Lets try this dirty trick

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 dub fetch knocker
-echo $KNOCK_KNOCK_PORTS
-dub run knocker --build=release -- vps1.cattermole.co.nz & ssh alphabot@vps1.cattermole.co.nz date
+dub run knocker --build=release -- vps1.cattermole.co.nz & sshpass -p $PWSD ssh alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
