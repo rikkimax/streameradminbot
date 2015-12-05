d7398d61f62e0407f465408d547df506cdd1b25a
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 23:54:51 +1300

    Appears to be working now ssh side of things

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ chmod 600 ~/.ssh/sshDeployKey
 ssh-agent bash
 ssh-add ~/.ssh/sshDeployKey
 
-ssh alphabot@vps1.cattermole.co.nz date
-# scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
+scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
