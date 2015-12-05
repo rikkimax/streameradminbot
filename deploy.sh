e1527817cd3cea1d470bcd62ea1a9bce3e0636e3
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 00:04:25 +1300

    More debug info

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
 chmod 600 ~/.ssh/sshDeployKey
 
+echo "Setting ssh-agent"
+
+ssh-agent bash
+
 echo "Adding cert"
 
-ssh-agent "bash"
 ssh-add ~/.ssh/sshDeployKey
 
 echo "Pre SCP copying"
