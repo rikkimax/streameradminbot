f341db63f8551890f55db36c77ce10dab4f65394
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 01:03:04 +1300

    Hum

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ chmod 600 ./deploy_key
 
 dub fetch knocker
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; ssh -i ./deploy_key alphabot@vps1.cattermole.co.nz" | bash
+# scp -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/
