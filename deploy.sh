c4c0e0e365426e2a9bdabf8c1f78ebf066ffb04a
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 22:58:26 +1300

    Lets see what is going on

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 dub fetch knocker
-dub run knocker --build=release -- vps1.cattermole.co.nz 
-echo "Deploying to server"
-ssh alphabot@vps1.cattermole.co.nz date
+echo KNOCK_KNOCK_PORTS
+dub run knocker --build=release -- vps1.cattermole.co.nz & ssh alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
