259e871244afd75ff0f8b17c3e86c7871aeaf938
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 23:53:27 +1300

    Sigh come on work

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
 chmod 600 ~/.ssh/sshDeployKey
-ssh -i ~/.ssh/sshDeployKey alphabot@vps1.cattermole.co.nz date
+
+ssh-agent bash
+ssh-add ~/.ssh/sshDeployKey
+
+ssh alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
