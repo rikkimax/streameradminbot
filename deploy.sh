0bdc7a05a526d25dc4ec3fa158b065661e18cda2
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 22:59:41 +1300

    Gwah woops forgot the $ in the variable

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 dub fetch knocker
-echo KNOCK_KNOCK_PORTS
+echo $KNOCK_KNOCK_PORTS
 dub run knocker --build=release -- vps1.cattermole.co.nz & ssh alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
