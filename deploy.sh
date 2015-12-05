e7fefa59da148a726644bd668ca99aef5c4ef0a0
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 22:37:05 +1300

    Okay maybe chaining is failing

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 dub fetch knocker
-dub run knocker --build=release -- vps1.cattermole.co.nz & echo "Deploying to server" & scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
+dub run knocker --build=release -- vps1.cattermole.co.nz 
+echo "Deploying to server"
+scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
