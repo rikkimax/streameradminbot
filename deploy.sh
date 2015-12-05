2ed1e7255dfc72928f1272d254bc4a615ac68ca7
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 01:22:25 +1300

    another try

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ chmod 600 ./deploy_key
 
 dub fetch knocker
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) -o \"StrictHostKeyChecking no\" alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) -o stricthostkeychecking=no alphabot@vps1.cattermole.co.nz:~/" | bash
