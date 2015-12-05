324759f0ddeb95f850fed1d9edb932c16bc85de0
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 23:18:49 +1300

    Removing all knocker requirements

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-dub fetch knocker
-dub run knocker --build=release -- vps1.cattermole.co.nz & sshpass -p $PWSD ssh alphabot@vps1.cattermole.co.nz date
+ssh alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
