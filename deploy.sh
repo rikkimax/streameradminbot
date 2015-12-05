763237e1b1b5b2a0da1fb3c7555dfa797fdd1744
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 00:19:22 +1300

    gha

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,4 @@ restorecon -R -v ~/.ssh
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
 chmod 600 ~/.ssh/sshDeployKey
 
-echo "Adding cert"
-
-ssh-add ~/.ssh/sshDeployKey
-
-echo "Pre SCP copying"
-echo "Setting ssh-agent"
-
-ssh-agent echo "scp -i ~/.ssh/sshDeployKey -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
-
-echo "Post SCP copying"
+ssh-agent echo "ssh-add ~/.ssh/sshDeployKey ; scp -i ~/.ssh/sshDeployKey -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
