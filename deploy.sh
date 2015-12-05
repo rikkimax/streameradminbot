f5789284327a470ac3abf07822d879068cc65887
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 23:45:48 +1300

    Locking down private key

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
+chmod 600 ~/.ssh/sshDeployKey
 ssh -i ~/.ssh/sshDeployKey alphabot@vps1.cattermole.co.nz date
 # scp -o ConnectTimeout=25 -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
