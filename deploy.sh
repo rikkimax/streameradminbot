53b001d2d1e2b8d3b7135f216d36264cd2f82fb6
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 01:34:04 +1300

    Ugh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@ chmod 600 ./deploy_key
 
 dub fetch knocker
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp  -o CheckHostIP=no -o UserKnownHostsFile=/dev/null -v -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
+echo "Host vps1.cattermole.co.nz" >> ~/.ssh/config
+echo "  UserKnownHostsFile /dev/null" >> ~/.ssh/config
+echo "  StrictHostKeyChecking no" >> ~/.ssh/config
+
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp  -v -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
