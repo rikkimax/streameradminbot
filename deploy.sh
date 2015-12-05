ad5273d9e9ecddb078aa8dbe987fefc1e77a3fd7
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 00:22:08 +1300

    31

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ restorecon -R -v ~/.ssh
 echo $DEPLOY_KEY > ~/.ssh/sshDeployKey
 chmod 600 ~/.ssh/sshDeployKey
 
-ssh-agent echo "ssh-add ~/.ssh/sshDeployKey ; scp -i ~/.ssh/sshDeployKey -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ~/.ssh/sshDeployKey ; scp -v -i ~/.ssh/sshDeployKey -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
