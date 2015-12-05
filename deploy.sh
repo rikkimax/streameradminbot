c7ef83a8be098455deeb352bd9db78d4a9778017
rikki cattermole <pbdadmin@gmail.com>
2015-12-05 22:35:20 +1300

    Now it should work

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 dub fetch knocker
-dub run knocker --build=release -- vps1.cattermole.co.nz
+dub run knocker --build=release -- vps1.cattermole.co.nz & echo "Deploying to server" & scp -r $(pwd) alphabot@vps1.cattermole.co.nz:~/program
