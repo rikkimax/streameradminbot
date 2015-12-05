376b778d9f8db13245a205fb72c5a2337693ae92
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 01:49:26 +1300

    Should be fairly secured now

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ echo "Host vps1.cattermole.co.nz" >> ~/.ssh/config
 echo "  UserKnownHostsFile /dev/null" >> ~/.ssh/config
 echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp  -v -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
