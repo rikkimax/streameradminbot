8834f47d69719e72491fd9c158462f84683fc3e8
rikki cattermole <pbdadmin@gmail.com>
2015-12-06 01:28:23 +1300

    debug time

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ chmod 600 ./deploy_key
 
 dub fetch knocker
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key -r $(pwd) -o CheckHostIP=no -o UserKnownHostsFile=/dev/null alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp  -o CheckHostIP=no -o UserKnownHostsFile=/dev/null -v -i ./deploy_key -r $(pwd) alphabot@vps1.cattermole.co.nz:~/" | bash
