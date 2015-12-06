15f15e5d2d206a44bbedbadbc593ddfd0416140d
rikki cattermole <pbdadmin@gmail.com>
2015-12-07 01:46:26 +1300

    changing place to upload

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo "Host vps1.cattermole.co.nz" >> ~/.ssh/config
 echo "  UserKnownHostsFile /dev/null" >> ~/.ssh/config
 echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 
-tar -zcvf from_travis.tar.gz $(ls -rt)
+tar -zcvf compressed.tar.gz $(ls -rt)
 
-ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key ./from_travis.tar.gz alphabot@vps1.cattermole.co.nz:~/" | bash
+ssh-agent echo "ssh-add ./deploy_key ; dub run knocker --build=release -- vps1.cattermole.co.nz ; scp -i ./deploy_key ./compressed.tar.gz alphabot@vps1.cattermole.co.nz:~/from_travis/" | bash
